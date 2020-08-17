f6f16ae837f0ebd9ef7f1a7a59fe70a53aff0eef
Евгений Нейт <61351123+xN8Tx@users.noreply.github.com>
2020-08-18 00:02:23 +0700

    Add files via upload

---
+++ test/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function functionName() {
+  $('.header__burger').click(function(){
+        $('.header__burger, .header__menu').toggleClass('active');
+	$('body').toggleClass('lock');
+});
+})
